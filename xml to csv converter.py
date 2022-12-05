beb90c5fd6c952ad8f55b9795747ec0f676a9f4d
Frederik Bezuidenhout <jaco.bezuidenhout11@gmail.com>
2022-12-05 15:34:50 +1000

    Finish Converter and add GUI. Not tested

--- xml to csv converter.py
+++ xml to csv converter.py
@@ -1,36 +1,80 @@
-#xml to csv converter
-#TODO: add loop to find file names
-#TODO: add loop to convert each file
-
-
 import xml.etree.ElementTree as Xet
-import pandas as pd
-  
-cols = ["name", "phone", "email", "date", "country"]
-rows = []
-fileNames = ["file1.xml", "file2.xml", "file3.xml"]
+import csv
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QCheckBox
 
-xmlparse = Xet.parse('filename goes here')
-root = xmlparse.getroot()
-for i in root:
-    name = i.find("name").text
-    phone = i.find("phone").text
-    email = i.find("email").text
-    date = i.find("date").text
-    country = i.find("country").text
-  
-    rows.append({"name": name,
-                 "phone": phone,
-                 "email": email,
-                 "date": date,
-                 "country": country})
-  
-df = pd.DataFrame(rows, columns=cols)
-  
+class XMLConverter(QWidget):
+    def __init__(self):
+        super().__init__()
 
-df.to_csv('output.csv')
-def main():
-    pass
+        # Create a button to start the conversion process
+        self.convert_button = QPushButton("Convert XML Files")
+        self.convert_button.clicked.connect(self.convert_xml_files)
 
-if __name__ == "__main__":
-    main()
+        # Create a line edit to enter the folder path
+        self.folder_path_edit = QLineEdit()
+
+        # Create a layout to hold the widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.folder_path_edit)
+        layout.addWidget(self.convert_button)
+
+        # Parse one of the XML files to determine the columns that exist
+        folder_path = self.folder_path_edit.text()
+        file_names = os.listdir(folder_path)
+        xmlparse = Xet.parse(os.path.join(folder_path, file_names[0]))
+        root = xmlparse.getroot()
+
+        # Create a list of the columns to include in the CSV file
+        self.columns = []
+        for child in root:
+            for subchild in child:
+                if subchild.tag not in self.columns:
+                    self.columns.append(subchild.tag)
+
+        # Create a checkbox for each column
+        self.checkboxes = []
+        for column in self.columns:
+            checkbox = QCheckBox(column)
+            self.checkboxes.append(checkbox)
+            layout.addWidget(checkbox)
+
+        self.setLayout(layout)
+
+    def convert_xml_files(self):
+        # Get the folder path from the line edit
+        folder_path = self.folder_path_edit.text()
+
+        # Get the names of all the files in the folder
+        file_names = os.listdir(folder_path)
+
+        # Create a list of the selected columns
+        selected_columns = []
+        for i, checkbox in enumerate(self.checkboxes):
+            if checkbox.isChecked():
+                selected_columns.append(self.columns[i])
+
+        with open('output.csv', 'w', newline='') as csvfile:
+            # Create a DictWriter object to write the data to the CSV file
+            writer = csv.DictWriter(csvfile, fieldnames=selected_columns)
+            writer.writeheader()
+
+            for file_name in file_names:
+                # Check if the file is an XML file
+                if file_name.endswith(".xml"):
+                    # Parse the XML file and write the data to the CSV file
+                    xmlparse = Xet.parse(os.path.join(folder_path, file_name))
+                    root = xmlparse.getroot()
+                    for i in root:
+                        # Create a dictionary containing the XML data
+                        data = {}
+                        for column in selected_columns:
+                            data[column] = i.find(column).text
+                        # Write the dictionary to the CSV file
+                        writer.writerow(data)
+
+# Create an instance of the GUI and show it
+app = QApplication([])
+gui = XMLConverter()
+gui.show()
+app.exec_()
