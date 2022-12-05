a8b0dbb47afefb99ed304c8b1323816e67918923
Frederik Bezuidenhout <jaco.bezuidenhout11@gmail.com>
2022-12-05 19:53:29 +1000

    Tested with sample data & confirmed working GUI

--- xml to csv converter.py
+++ xml to csv converter.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as Xet
 import csv
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QCheckBox
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QCheckBox, QLabel
 
 class XMLConverter(QWidget):
     def __init__(self):
@@ -13,33 +13,53 @@ class XMLConverter(QWidget):
 
         # Create a line edit to enter the folder path
         self.folder_path_edit = QLineEdit()
-
+        self.analyse_button = QPushButton("Analyse")
+        self.analyse_button.clicked.connect(self.analyse_data)
+        self.status_text = QLabel("Waiting for folder path")
+        self.sub_status_text = QLabel("")
+        self.columns = []
+        self.checkboxes=[]
+        
         # Create a layout to hold the widgets
-        layout = QVBoxLayout()
-        layout.addWidget(self.folder_path_edit)
-        layout.addWidget(self.convert_button)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.status_text)
+        self.layout.addWidget(self.sub_status_text)
+        self.layout.addWidget(self.folder_path_edit)
+        self.layout.addWidget(self.analyse_button)
+        
 
+
+        self.setLayout(self.layout)
+
+    def analyse_data(self):
         # Parse one of the XML files to determine the columns that exist
         folder_path = self.folder_path_edit.text()
         file_names = os.listdir(folder_path)
-        xmlparse = Xet.parse(os.path.join(folder_path, file_names[0]))
+        first_file = os.path.join(folder_path, file_names[0])
+        #print(first_file)
+        xmlparse = Xet.parse(first_file)
         root = xmlparse.getroot()
 
         # Create a list of the columns to include in the CSV file
-        self.columns = []
+        self.columns.append(root.tag)
         for child in root:
+            if child.tag not in self.columns:
+                self.columns.append(child.tag)
             for subchild in child:
                 if subchild.tag not in self.columns:
                     self.columns.append(subchild.tag)
-
-        # Create a checkbox for each column
-        self.checkboxes = []
+        
+        
         for column in self.columns:
             checkbox = QCheckBox(column)
             self.checkboxes.append(checkbox)
-            layout.addWidget(checkbox)
+            self.layout.addWidget(checkbox)
 
-        self.setLayout(layout)
+        self.layout.addWidget(self.convert_button)
+        self.status_text.setText("Select the columns to include in the CSV file")
+        
+
+    
 
     def convert_xml_files(self):
         # Get the folder path from the line edit
@@ -58,8 +78,14 @@ class XMLConverter(QWidget):
             # Create a DictWriter object to write the data to the CSV file
             writer = csv.DictWriter(csvfile, fieldnames=selected_columns)
             writer.writeheader()
-
+            num_files = len(file_names)
+            current_file = 0
+            current_file_name = ""
+            current_column = ""
             for file_name in file_names:
+                current_file += 1
+                current_file_name = file_name
+                self.status_text.setText(f"Converting file {current_file} of {num_files}")
                 # Check if the file is an XML file
                 if file_name.endswith(".xml"):
                     # Parse the XML file and write the data to the CSV file
@@ -69,12 +95,21 @@ class XMLConverter(QWidget):
                         # Create a dictionary containing the XML data
                         data = {}
                         for column in selected_columns:
-                            data[column] = i.find(column).text
+                            self.sub_status_text.setText(f"Current File: {current_file_name} | Processing column: {column}")
+                            print(f"Current File: {current_file_name} | Processing column: {column}")
+                            # Check if the element exists
+                            element = i.find(column)
+                            if element is not None:
+                            # If the element exists, add its text to the data dictionary
+                                data[column] = element.text
+  
                         # Write the dictionary to the CSV file
                         writer.writerow(data)
-
+            self.status_text.setText(f"Converted {current_file} of {num_files}")
+            self.sub_status_text.setText("")
+            self.layout.addWidget(QLabel("Done!"))
 # Create an instance of the GUI and show it
 app = QApplication([])
-gui = XMLConverter()
-gui.show()
-app.exec_()
+window = XMLConverter()
+window.show()
+app.exec()
